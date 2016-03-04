6f4126f219bab1cf7df26eac5fa979f4a2726bea
Brandon Dunne <bdunne@redhat.com>
2016-03-04 18:27:43 -0500

    PAUSED: Use `git resume` to continue working.

--- app/workers/commit_monitor_handlers/batch/github_pr_commenter/diff_checker.rb
+++ app/workers/commit_monitor_handlers/batch/github_pr_commenter/diff_checker.rb
@@ -22,8 +22,8 @@ module CommitMonitorHandlers::Batch
     end
 
     def process_diff
-      # diff = branch.git_diff
-      diff = Branch.first.git_diff
+      diff = branch.git_diff
+      # diff = Branch.first.git_diff
       diff.patches.each { |patch| process_patch(patch) }
     end
 
@@ -39,24 +39,23 @@ module CommitMonitorHandlers::Batch
       return unless line.addition?
       location = "#{parent_patch.delta.new_file[:path]}:#{line.new_lineno}"
       content  = line.content.downcase
-      check_line_blacklisted(content, location)
-      check_line_puts(content, location)
+      check_line(content, location)
     end
 
-    def add_offense(severity, offense, location)
-      @offenses << "#{severity} Detected *\"#{offense}\"* at #{location}"
+    def add_offense(severity, offender, location)
+      @offenses << "#{severity} **\"#{offender}\"** Detected at #{location}"
     end
 
-    def check_line_blacklisted(content, location)
-      return if Settings.diff_checker.blacklisted_strings.except.try(:any?) { |except| location.start_with?(except) }
-      Settings.diff_checker.blacklisted_strings.matchers.each do |matcher|
-        add_offense(":black_circle:", matcher, location) if content.include?(matcher)
+    def settings
+      @settings ||= Settings.diff_checker
+    end
+
+    def check_line(content, location)
+      settings.each do |offender, options|
+        next if options.except.try(:any?) { |except| location.start_with?(except) }
+        string = offender.to_s
+        add_offense(options.severity, string, location) if content.include?(string)
       end
-    end
-
-    def check_line_puts(content, location)
-      return if Settings.diff_checker.puts.except.any? { |except| location.start_with?(except) }
-      add_offense(":red_circle:", "puts", location) if content.include?("puts")
     end
   end
 end
